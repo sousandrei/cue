f323992d2bae7debb6c8b1abe8c94fdc5c944284
Andrei Sousa <sousandrei@gmail.com>
2026-02-08 20:16:48 +0100

    chore: config in module

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::{fs, sync::Mutex};
+use tauri::State;
+
+use crate::download;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Config {
@@ -70,6 +73,29 @@ pub fn save_config(config: &Config) -> Result<(), String> {
 
     fs::write(&config_path, config_str)
         .map_err(|e| format!("Failed to write config file: {}", e))?;
+
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn update_config(
+    state: State<'_, Mutex<Config>>,
+    app: tauri::AppHandle,
+    new_config: Config,
+) -> Result<(), String> {
+    // Save to file
+    save_config(&new_config).map_err(|e| e.to_string())?;
+
+    // Update state
+    {
+        let mut config = state.lock().unwrap();
+        *config = new_config.clone();
+    }
+
+    // Ensure yt-dlp version
+    download::ensure_ytdlp(&app, &new_config.yt_dlp_version)
+        .await
+        .map_err(|e| e.to_string())?;
 
     Ok(())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,30 +4,7 @@ mod download;
 
 use db::Database;
 use std::sync::Mutex;
-use tauri::{Manager, State};
-
-#[tauri::command]
-async fn update_config(
-    state: State<'_, Mutex<config::Config>>,
-    app: tauri::AppHandle,
-    new_config: config::Config,
-) -> Result<(), String> {
-    // Save to file
-    config::save_config(&new_config).map_err(|e| e.to_string())?;
-
-    // Update state
-    {
-        let mut config = state.lock().unwrap();
-        *config = new_config.clone();
-    }
-
-    // Ensure yt-dlp version
-    download::ensure_ytdlp(&app, &new_config.yt_dlp_version)
-        .await
-        .map_err(|e| e.to_string())?;
-
-    Ok(())
-}
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -54,7 +31,7 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            update_config,
+            config::update_config,
             db::commands::add_song,
             db::commands::remove_song,
             db::commands::edit_song,
